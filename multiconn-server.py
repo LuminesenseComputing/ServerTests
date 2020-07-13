db8cfa46b8c70fc8b7469223d9fb39e7bf127779
ChristopherAlexiev <chris340a@gmail.com>
2020-07-13 01:40:18 -0400

    some structural changes

--- multiconn-server.py
+++ multiconn-server.py
@@ -12,7 +12,7 @@ class lightModule:
         self.port = port
         print("    Light ", self.port, " is now ONLINE.")
 
-    def changeState(self):
+    def changeState(self):#must add "turning on/off" states
         if self.state == 0:
             self.state = 1
             print("    Light ", self.port, " is now ON.")
@@ -27,7 +27,7 @@ def accept_wrapper(sock, lightModuleDict):
     conn, addr = sock.accept()  # Should be ready to read
     print("accepted connection from", addr)
     conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    data = types.SimpleNamespace(addr=addr, inb=b"", messages=[], outb=b"")
     events = selectors.EVENT_READ | selectors.EVENT_WRITE
     sel.register(conn, events, data=data)
     lightModuleDict[addr[1]] = lightModule(addr[1])
@@ -36,12 +36,13 @@ def service_connection(key, mask, lightModuleDict, changeState):
     sock = key.fileobj
     data = key.data
     if changeState == True: #really sketch. Just demonstrating ideas
-        data.outb  += b"CHANGE STATE"
+        data.messages  += [b"CHANGE STATE"]
         lightModuleDict[data.addr[1]].changeState()
     if mask & selectors.EVENT_READ:
         recv_data = sock.recv(1024)  # Should be ready to read
         if recv_data:
-            data.outb += recv_data
+            #data.outb += recv_data
+            pass
         else:
             lightModuleDict[data.addr[1]].closeLight()
             lightModuleDict.pop(data.addr[1])
@@ -50,17 +51,20 @@ def service_connection(key, mask, lightModuleDict, changeState):
             sock.close()
 
     if mask & selectors.EVENT_WRITE:
+        if not data.outb and data.messages:
+            data.outb = data.messages.pop()
         if data.outb:
-            print("echoing", repr(data.outb), "to", data.addr)
+            print("Sending", repr(data.outb), "to", data.addr)
             sent = sock.send(data.outb)  # Should be ready to write
             data.outb = data.outb[sent:]
 
 
-if len(sys.argv) != 3:
-    print("usage:", sys.argv[0], "<host> <port>")
+if len(sys.argv) != 1:
+    print("usage:", sys.argv[0], "no input arguments")
     sys.exit(1)
 
-host, port = sys.argv[1], int(sys.argv[2])
+host = "192.168.4.1"
+port = 50007
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lsock.bind((host, port))
 lsock.listen()
